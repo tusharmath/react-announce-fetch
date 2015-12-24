2cab7344db35697f01897ccc5b87ed4225f7dc92
Tushar Mathur <tusharmath@gmail.com>
2015-12-24 10:49:51 +0530

    use storeStream for hydration

--- src/createDataStore.js
+++ src/createDataStore.js
@@ -10,17 +10,16 @@ const Subject = Rx.Subject
 const createStoreAsStream = require('reactive-storage').createStoreAsStream
 
 module.exports = function (stream, fetcher) {
-  var componentMounted = 0
   const response = new Subject()
   const reload = new Subject()
-  const hydrate = new Subject()
+  const hydrate = createStoreAsStream(0)
   const store = createStoreAsStream(new Immutable({}))
   stream
     .filter(Boolean)
     .subscribe(x => store.update(y => y.merge(x)))
   store
     .getStream()
-    .combineLatest(hydrate, (a, b) => ({a, b}))
+    .combineLatest(hydrate.getStream(), (a, b) => ({a, b}))
     .filter(x => x.b > 0)
     .map(x => x.a)
     .combineLatest(reload.startWith(null), _.identity)
@@ -28,10 +27,7 @@ module.exports = function (stream, fetcher) {
     .subscribe(response)
   return {
     getStream: () => response,
-    hydrate: x => {
-      componentMounted += x
-      hydrate.onNext(componentMounted)
-    },
+    hydrate: x => hydrate.update(v => v + x),
     reload: () => reload.onNext(null)
   }
 }

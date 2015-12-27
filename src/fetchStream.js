577cec47bd3d71045d105b8a1b7af7cb7efb66c2
Tushar Mathur <tusharmath@gmail.com>
2015-12-27 13:47:35 +0530

    use global fetch

--- src/fetchStream.js
+++ src/fetchStream.js
@@ -5,6 +5,6 @@ module.exports = params => {
   const url = params.url
   const options = _.omit(params, 'url')
   const parseJSON = x => Rx.Observable.fromPromise(x.json()).map(json => ({json, response: x}))
-  const fetchAsObservable = (url, options) => Rx.Observable.fromPromise(params.fetch(url, options))
+  const fetchAsObservable = (url, options) => Rx.Observable.fromPromise(fetch(url, options))
   return fetchAsObservable(url, options).flatMap(parseJSON)
 }

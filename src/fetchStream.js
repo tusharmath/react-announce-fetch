eff12bc276df465220656e84c24d2e51df40d8f1
Tushar Mathur <tusharmath@gmail.com>
2015-12-28 18:47:28 +0530

    parse json and save

--- src/fetchStream.js
+++ src/fetchStream.js
@@ -1,4 +1,11 @@
 const _ = require('lodash')
 const Rx = require('rx')
 
-module.exports = params => Rx.Observable.fromPromise(fetch(params.url, _.omit(params, 'url')))
+module.exports = params => {
+  const url = params.url
+  const options = _.omit(params, 'url')
+  const responseParams = ['body', 'bodyUsed', 'headers', 'ok', 'status', 'statusText', 'type', 'url']
+  const selectResponseParams = _.partialRight(_.pick, responseParams)
+  const parseJSON = x => Rx.Observable.fromPromise(x.json()).map(json => _.assign({json}, selectResponseParams))
+  return Rx.Observable.fromPromise(params.fetch(url, options)).flatMap(parseJSON)
+}

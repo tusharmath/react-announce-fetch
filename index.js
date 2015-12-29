b3fcaccfe8ae84bf536f41c3e0a54c46acf049dd
Tushar Mathur <tusharmath@gmail.com>
2015-12-29 14:51:33 +0530

    remove imports for hydrate

--- index.js
+++ index.js
@@ -6,9 +6,7 @@
 const _ = require('lodash')
 const createDataStore = require('./src/createDataStore')
 const fetchStream = require('./src/fetchStream')
-const hydrate = require('./src/hydrate')
 
 module.exports = {
-  createDataStore: (requestStream, initialValue) => createDataStore(requestStream, initialValue, fetchStream),
-  hydrate
+  createDataStore: (requestStream, initialValue) => createDataStore(requestStream, initialValue, fetchStream)
 }

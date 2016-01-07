d27e542b1774bd9f7f13235506bd3cd9ca953039
Tushar Mathur <tusharmath@gmail.com>
2016-01-08 00:12:38 +0530

    rename variable

--- src/createDataStore.js
+++ src/createDataStore.js
@@ -5,7 +5,7 @@
 
 const Rx = require('rx')
 const _ = require('lodash')
-const createStoreAsStream = require('reactive-storage').createStoreStream
+const createStoreStream = require('reactive-storage').createStoreStream
 
 // TODO: Remove initial value. Startwith is a good alternative to use
 module.exports = function (requestStream, initialValue, fetcher) {
@@ -13,7 +13,7 @@ module.exports = function (requestStream, initialValue, fetcher) {
   const response = new Rx.BehaviorSubject(initialValue)
   const reload = new Rx.Subject()
   const state = new Rx.Subject()
-  const hydrate = createStoreAsStream(0)
+  const hydrate = createStoreStream(0)
   Rx.Observable.merge(
     lifeCycleObserver.filter(x => x.event === 'WILL_MOUNT').map(1),
     lifeCycleObserver.filter(x => x.event === 'WILL_UNMOUNT').map(-1)

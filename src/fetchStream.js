96fc01b4af2684f5a52ea1d9ea5cfaec4c291ea8
Tushar Mathur <tusharmath@gmail.com>
2016-01-11 20:48:00 +0530

    fix fetch params

--- src/fetchStream.js
+++ src/fetchStream.js
@@ -1,11 +1,12 @@
 const _ = require('lodash')
 const Rx = require('rx')
 
+// TODO: Add unit tests
 module.exports = params => {
   const url = params.url
   const options = _.omit(params, 'url')
   const responseParams = ['body', 'bodyUsed', 'headers', 'ok', 'status', 'statusText', 'type', 'url']
   const selectResponseParams = _.partialRight(_.pick, responseParams)
-  const parseJSON = x => Rx.Observable.fromPromise(x.json()).map(json => _.assign({json}, selectResponseParams))
+  const parseJSON = x => Rx.Observable.fromPromise(x.json()).map(json => _.assign({json}, selectResponseParams(x)))
   return Rx.Observable.fromPromise(fetch(url, options)).flatMap(parseJSON)
 }

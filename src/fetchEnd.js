3b91daa5ef9b072036d6e21e27f26511b9e4de06
Tushar Mathur <tusharmath@gmail.com>
2016-02-25 01:53:53 +0530

    fix(fetchEnd): use fetch API via apply method

--- src/fetchEnd.js
+++ src/fetchEnd.js
@@ -1,9 +1,13 @@
 module.exports = (fetch, com) => {
-  const request = com.filter(x => x.event === 'FETCH_BEGIN').map(x => x.args[0])
-  const reload = com.filter(x => x.event === 'RELOAD').startWith(null)
+  const request = com
+    .filter(x => x.event === 'FETCH_BEGIN')
+
+  const reload = com
+    .filter(x => x.event === 'RELOAD')
+    .startWith(null)
 
   return request
     .combineLatest(reload, a => a)
-    .flatMap(x => fetch(x))
+    .flatMap(x => fetch.apply(null, x.args))
     .map(x => ({event: 'FETCH_END', args: [x]}))
 }
--- test/test.fetchEnd.js
+++ test/test.fetchEnd.js
@@ -5,18 +5,18 @@ const {onNext} = ReactiveTest
 
 test(t => {
   const out = []
-  const fetch = x => [x]
+  const fetch = (url, options) => [url + options.opt]
   const sh = new TestScheduler()
   const com = sh.createHotObservable(
-    onNext(210, {event: 'FETCH_BEGIN', args: ['req0']}),
-    onNext(220, {event: 'FETCH_BEGIN', args: ['req1']}),
+    onNext(210, {event: 'FETCH_BEGIN', args: ['/url-0', {opt: 0}]}),
+    onNext(220, {event: 'FETCH_BEGIN', args: ['/url-1', {opt: 1}]}),
     onNext(230, {event: 'RELOAD'})
   )
   e(fetch, com).subscribe(x => out.push(x))
   sh.start()
   t.same(out, [
-    {event: 'FETCH_END', args: ['req0']},
-    {event: 'FETCH_END', args: ['req1']},
-    {event: 'FETCH_END', args: ['req1']}
+    {event: 'FETCH_END', args: ['/url-00']},
+    {event: 'FETCH_END', args: ['/url-11']},
+    {event: 'FETCH_END', args: ['/url-11']}
   ])
 })

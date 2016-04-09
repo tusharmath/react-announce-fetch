f6fea1336aca48b648613b23d179514ec3c40716
Tushar Mathur <tusharmath@gmail.com>
2016-04-09 18:52:50 +0530

    refactor(isHydrated): use scan() operator instead of a variable

--- src/main.js
+++ src/main.js
@@ -20,13 +20,12 @@ e.xhr = (fetch, request, observer) => Rx.Observable.merge(
 )
 
 e.isHydrated = events => {
-  var hydrationCount = 0
   const mount = events.filter(x => x.event === 'WILL_MOUNT').map(1)
   const unmount = events.filter(x => x.event === 'WILL_UNMOUNT').map(-1)
   return Rx.Observable
     .merge(mount, unmount).startWith(0)
-    .tap(x => hydrationCount += x)
-    .map(() => hydrationCount > 0)
+    .scan((m, x) => x + m, 0)
+    .map(x => x > 0)
     .distinctUntilChanged()
 }
 

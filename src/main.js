de44eac2b83098989f72c45250233e055b1f4703
Tushar Mathur <tusharmath@gmail.com>
2016-04-11 15:59:53 +0530

    fix(main): logical fallacy

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
  */
 'use strict'
 
+const ReactiveStore = require('reactive-storage')
 const Rx = require('rx')
 const _ = require('funjector')
+const targ = require('argtoob')
 const e = exports
 const slice = (x, i) => Array.prototype.slice.call(x, i)
 
@@ -15,9 +17,21 @@ e.toObservable = fetch => function () {
 
 e.create = _.partial((toObservable, request, fetch) => {
   const fetchO = toObservable(fetch)
-  const out = request
+  const refCount = ReactiveStore.create(0)
+  const responses = request
+    .map(x => x.slice(0))
+    .combineLatest(refCount.stream, targ('request', 'refCount'))
+    .filter(x => x.refCount > 0)
+    .pluck('request')
+    .distinctUntilChanged(x => x, (a, b) => a === b)
     .flatMap(x => fetchO.apply(null, x))
-    .replay(1)
-  out.connect()
-  return out
+    .publish()
+  responses.connect()
+  const replay = responses.replay(0, 1)
+  replay.connect()
+  return Rx.Observable.create(observer => {
+    refCount.set(x => x + 1)
+    replay.subscribe(observer)
+    return () => refCount.set(x => x - 1)
+  })
 }, e.toObservable)
--- test/test.main.js
+++ test/test.main.js
@@ -4,52 +4,118 @@
 
 'use strict'
 import {spy} from 'sinon'
-import {TestScheduler, ReactiveTest} from 'rx'
+import {TestScheduler, ReactiveTest, Observable} from 'rx'
 import test from 'ava'
 import {create} from '../src/main'
 import {call} from 'funjector'
 const {onNext, onCompleted} = ReactiveTest
 const identity = x => x
 
-test('calls fetch', t => {
+test('fetch:callCount: one per request', t => {
+  // Setup
   const fetch = spy(identity)
   const toObservable = () => fetch
   const sh = new TestScheduler()
+
   const request = sh.createHotObservable(
     onNext(210, ['a', 1]),
     onNext(220, ['b', 2])
   )
-  call(create, toObservable, request)
-    .subscribe(identity)
-  sh.start()
+
+  // Begin@200
+  const store = call(create, toObservable, request)
+  sh.startScheduler(() => store)
+
+  // Assert
+  t.is(fetch.callCount, 2)
   t.true(fetch.getCall(0).calledWith('a', 1))
   t.true(fetch.getCall(1).calledWith('b', 2))
 })
 
-test('multiple subscriptions', t => {
-  const fetch = spy(identity)
+test('fetch:callCount: multiple-subscription', t => {
+  // Setup
+  const fetch = spy(x => [x + '-response'])
   const toObservable = () => fetch
   const sh = new TestScheduler()
+  const observer0 = sh.createObserver()
+  const observer1 = sh.createObserver()
   const request = sh.createHotObservable(
     onNext(210, ['a', 1]),
     onNext(220, ['b', 2])
   )
+
   const store = call(create, toObservable, request)
-  store.subscribe(identity)
-  store.subscribe(identity)
+
+  // Begin@200
+  sh.scheduleAbsolute(null, 200, () => store.subscribe(observer0))
+  sh.scheduleAbsolute(null, 230, () => store.subscribe(observer1))
   sh.start()
+
+  // Assert
   t.is(fetch.callCount, 2)
+  t.true(fetch.getCall(0).calledWith('a', 1))
+  t.true(fetch.getCall(1).calledWith('b', 2))
+  t.deepEqual(observer0.messages, [onNext(210, 'a-response'), onNext(220, 'b-response')])
+  t.deepEqual(observer1.messages, [onNext(230, 'b-response')])
 })
 
-test('must get last value', t => {
-  const fetch = spy(x => x)
+test('fetch:callCount:as-hot', t => {
+  // Setup
+  const fetch = spy(x => [x + '-response'])
   const toObservable = () => fetch
   const sh = new TestScheduler()
   const request = sh.createHotObservable(
-    onNext(100, ['a', 1]),
-    onNext(110, ['b', 2])
+    onNext(100, ['a0', 0]),
+    onNext(110, ['b0', 1])
   )
-  sh.startScheduler(() => call(create, toObservable, request))
+
+  // Begin@200
+  const store = call(create, toObservable, request)
+  const messages = sh.startScheduler(() => store).messages
+
+  // Assert
   t.is(fetch.callCount, 1)
-  t.true(fetch.calledWith('b', 2))
+  t.true(fetch.calledWith('b0', 1))
+  t.deepEqual(messages, [onNext(200, 'b0-response')])
+})
+
+test('cold-request:no-fetch', t => {
+  // Setup
+  const fetch = spy(x => [x + '-response'])
+  const toObservable = () => fetch
+  const sh = new TestScheduler()
+
+  // Begin@200
+  const store = call(create, toObservable, Observable.just(['a0', 0]))
+
+  // Initial Assert
+  t.is(fetch.callCount, 0)
+  const messages = sh.startScheduler(() => store).messages
+
+  // Assert
+  t.is(fetch.callCount, 1)
+  t.true(fetch.getCall(0).calledWith('a0', 0))
+  t.deepEqual(messages, [onNext(200, 'a0-response')])
+})
+
+test('fetch:reload', t => {
+  // Setup
+  const fetch = spy(x => [x + '-response'])
+  const toObservable = () => fetch
+  const sh = new TestScheduler()
+  const params = ['a0', 0]
+  const request = sh.createHotObservable(
+    onNext(200, params),
+    onNext(210, params)
+  )
+
+  // Begin@200
+  const store = call(create, toObservable, request)
+  const messages = sh.startScheduler(() => store).messages
+
+  // Assert
+  t.is(fetch.callCount, 2)
+  t.true(fetch.getCall(0).calledWith('a0', 0))
+  t.true(fetch.getCall(1).calledWith('a0', 0))
+  t.deepEqual(messages, [onNext(200, 'a0-response'), onNext(210, 'a0-response')])
 })

ef8fb5c40ddd56bd7ee236557871f0bf06105783
Tushar Mathur <tusharmath@gmail.com>
2016-04-14 18:42:44 +0530

    fix(create): fix: xhr is not a function

--- src/main.js
+++ src/main.js
@@ -4,20 +4,49 @@
 'use strict'
 
 const Rx = require('rx')
+const targs = require('argtoob')
 const _ = require('funjector')
 const e = exports
-const slice = (x, i) => Array.prototype.slice.call(x, i)
 
-e.toObservable = fetch => function () {
-  const args = slice(arguments)
-  return Rx.Observable.fromPromise(fetch.apply(null, args))
+e.xhr = (fetch, request, observer) => Rx.Observable.merge(
+  e.fetchBegin(request, observer),
+  e.fetchEnd(fetch, observer)
+)
+
+e.create = _.partial((xhr, fetch, request) => {
+  const observer = new Rx.Subject()
+  xhr(fetch, request, observer).subscribe(observer)
+  return observer
+}, e.xhr)
+
+e.isHydrated = events => {
+  const mount = events.filter(x => x.event === 'WILL_MOUNT').map(1)
+  const unmount = events.filter(x => x.event === 'WILL_UNMOUNT').map(-1)
+  return Rx.Observable
+    .merge(mount, unmount).startWith(0)
+    .scan((m, x) => x + m, 0)
+    .map(x => x > 0)
+    .distinctUntilChanged()
 }
 
-e.create = _.partial((toObservable, fetch, request) => {
-  const fetchO = toObservable(fetch)
-  const out = request
-    .flatMap(x => fetchO.apply(null, x))
-    .replay(1)
-  out.connect()
-  return out
-}, e.toObservable)
+e.getHydratedRequests = _.partial((isHydrated, request, com) => request
+  .combineLatest(isHydrated(com), targs('request', 'isHydrated'))
+  .filter(x => x.isHydrated)
+  .pluck('request'),
+  e.isHydrated
+)
+
+e.fetchBegin = _.partial((getHydratedRequests, req, com) => {
+  const reload = com
+    .filter(x => x.event === 'RELOAD')
+    .startWith(null)
+
+  return getHydratedRequests(req, com)
+    .map(x => ({event: 'REQUEST', args: x}))
+    .combineLatest(reload, a => a)
+}, e.getHydratedRequests)
+
+e.fetchEnd = (fetch, com) => com
+  .filter(x => x.event === 'REQUEST')
+  .flatMap(x => fetch.apply(null, x.args))
+  .map(x => ({event: 'RESPONSE', args: [x]}))
